ce73e42996e6c713b42fb9c8e9ad0606a45a3aa5
Denis Maley <dmalei@xbsoftware.com>
2017-02-25 22:58:01 +0300

    Init

---
+++ app/src/main/java/com/app/manics/activities/LoginActivity.java
@@ -0,0 +1,147 @@
+package com.app.manics.activities;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.app.manics.R;
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
+
+public class LoginActivity extends FragmentActivity {
+
+    private boolean isResumed = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        VKSdk.wakeUpSession(this, new VKCallback<VKSdk.LoginState>() {
+            @Override
+            public void onResult(VKSdk.LoginState res) {
+                if (isResumed) {
+                    switch (res) {
+                        case LoggedOut:
+                            showLogin();
+                            break;
+                        case LoggedIn:
+                            startChartActivity();
+                            break;
+                        case Pending:
+                            break;
+                        case Unknown:
+                            break;
+                    }
+                }
+            }
+
+            @Override
+            public void onError(VKError error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        isResumed = true;
+        if (VKSdk.isLoggedIn()) {
+            startChartActivity();
+        } else {
+            showLogin();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        isResumed = false;
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void showLogin() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, new LoginFragment())
+                .commitAllowingStateLoss();
+    }
+
+    public static class LoginFragment extends Fragment {
+
+        private static final String[] SCOPE = new String[]{
+                VKScope.FRIENDS,
+                VKScope.WALL,
+                VKScope.PHOTOS,
+                VKScope.NOHTTPS,
+                VKScope.MESSAGES,
+                VKScope.DOCS
+        };
+
+        public LoginFragment() {
+            super();
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View loginView = inflater.inflate(R.layout.fragment_login, container, false);
+            loginView.findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    VKSdk.login(getActivity(), SCOPE);
+                }
+            });
+            return loginView;
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        VKCallback<VKAccessToken> callback = new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                //post token to the server. return session
+                startChartActivity();
+            }
+
+            @Override
+            public void onError(VKError error) {
+                // User didn't pass Authorization
+                System.out.println("sds");
+            }
+        };
+
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, callback)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void startChartActivity() {
+        //send request to server
+        boolean analytics = true;
+        if (analytics) {
+            startActivity(createIntent(ContentActivity.class));
+            return;
+        }
+        startActivity(createIntent(ChatsActivity.class));
+
+    }
+
+    private Intent createIntent(Class aClass) {
+        Intent intent = new Intent(this, aClass);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        return intent;
+    }
+}
